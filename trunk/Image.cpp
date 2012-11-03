ad73ce8433254f385d145d1c423c4191a5afb42d
vladimir.coxall <vladimir.coxall@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-11-03 21:40:37 +0000

    create bites from image

--- trunk/Image.cpp
+++ trunk/Image.cpp
@@ -2,7 +2,7 @@
 #include "Image.h"
 
 //Constructor
-Image::Image()
+Image::Image(unsigned int m, unsigned int n)
 {}
 
 //Destructor
@@ -14,5 +14,33 @@ void Image::Read(char* filename)
 {}
 
 //Outputs bite of the image
-Image Image::Bite(unsigned int x, unsigned int y)
-{}
+Image Image::Bite( unsigned int x, unsigned int y, unsigned image_nRow, unsigned image_nCol )
+{
+
+	for( int i = 0; i < image_nRow; i++ ){
+		for( int j = 0; j < image_nCol; j++ ){
+	
+			bite_colSize = image_nCol - j;
+			bite_rowSize = image_nRow - i;
+	
+	
+			bite* current_bite  = (bite*) malloc( bite_rowSize * bite_colSize * sizeof(int) );
+
+			for( int k = row_pos; k < nRow; k++ ){
+				for( int m = col_pos; m < nCol; m++ ){
+
+					current_bite[ k * bite_colSize + m ] = image[ k * bite_colSize + m ];
+
+				}
+			}
+
+			
+
+			//	Add this bite into the array of bites
+			
+			bite_list.push_back( current_bite );
+
+		}
+	}
+}
+
