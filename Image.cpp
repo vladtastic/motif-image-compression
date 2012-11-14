de3aacafb44b0484194217dbc644efae49b1edf2
vladimir.coxall <vladimir.coxall@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-11-14 05:48:49 +0000

    implemented overloaded Image constructor

--- Image.cpp
+++ Image.cpp
@@ -21,6 +21,46 @@ Image::~Image()
 void Image::Read(char* filename)
 {}
 
+//Takes in array of pixel values, outputs corresponding Image object
+Image::Image( int* pix_val, struct bitmap_core* bcore ){
+
+	assert( pix_val != NULL);
+	assert( bcore != NULL);	
+
+	int dimW = bcore.biWidth;
+	int dimH = bcore.biHeight;
+
+	assert( dimW > 0 );
+	assert( dimH > 0 );
+
+	vector<int> tmp;
+
+	Image cur = Image();
+	
+	cur.WIDTH = dimW;
+	cur.HEIGHT = dimH;
+
+	
+	for( int i=0; i<dimH; i++){
+		for( int j=0; j<dimW; j++){
+
+			tmp.push_back( pix_val[i][j] );
+		}
+
+		cur.intensity.push_back(tmp);
+
+		tmp.clear();
+
+	}
+	
+	~tmp;
+
+	return cur;
+
+}
+
+	
+
 //	Outputs bite of the image
 Image Image::Bite( unsigned int x, unsigned int y )
 {
