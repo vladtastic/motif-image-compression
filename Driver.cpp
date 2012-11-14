d93cd0dd01ae23b3c8c3e33c2ad1b453f545c6f4
vladimir.coxall <vladimir.coxall@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-11-14 07:01:54 +0000

    bites & autocorrelations generated in a threaded approach (UNTESTED)

--- Driver.cpp
+++ Driver.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <omp.h>
 
 #include "Image.h"
 #include "Motif.h"
@@ -24,14 +25,99 @@ int main( int argc, char* argv[] ){
 
 
 
-	//	Process the bitmap
-
+	// bmp to Image object
 
 	struct bitmap_core*  core_info;
 
 	int* bitmap_ptr = LoadBmp( argv[1], core_info );
 
-		
+	//!!	core_info has height, are pixel array dims biWidth * biHeight?
+
+	int dimR = core_info.biHeight;
+	int dimC = core_info.biWidth;
+
+	Image current_img = Image( bitmap_ptr, core_info );	
 	
+	int num_bites = dimW * dimH;
+
 	
+
+
+	//	BEGIN THREADED REGION
+
+	#pragma omp parallel private(thread_id, num_threads, chunk_size, remainder, bite_list, meet_list)
+	{
+
+		int thread_id = omp_get_thread_num();
+		int num_threads = omp_get_num_threads();
+		int chunk_size = num_bites / num_threads;
+		int remainder = num_bites % num_threads;
+
+		queue<Image> bite_list;
+
+		#pragma omp for
+		{
+
+			/*
+				An Image has mn bites
+				Each thread takes a chunk (subset)
+				Each thread gets coords of each bite in subset
+				Adds bite to its personal queue			
+			*/
+					
+			
+			for( int i = thread_id * chunk_size; i < (thread_id + 1 ) * chunk_size; i++ ){
+
+				int pos_row = i % dimR;
+				int pos_col = i % dimC;
+
+				bite_list.push( current_img.Bite( pos_row, pos_col ) );
+
+			}
+
+
+			
+			// Last thread takes the leftovers
+	
+			if( thread_id == num_threads - 1 ){
+				
+				for( int i = (thread_id + 1 ) * chunk_size; i < ((thread_id + 1) * chunk_size) + remainder; i++ ){
+
+					int pos_row = i % dimR;
+					int pos_col = i % dimC;
+
+					bite_list.push( current_img.Bite( pos_row, pos_col ) );
+				
+				}
+
+			}
+		}
+
+
+
+		// Each thread has a subset of autocorrelations
+		queue<Motif> meet_list;
+
+		if( thread_id == num_threads - 1 ){
+			
+			chunk_size = chunk_size + remainder;
 		
+		}
+
+		#pragma omp for
+		{
+
+			for( int i = chunk_size; i > 0; i-- ){
+		
+				Motif tmp = Motif();
+				meet_list.push( tmp.Meet( &current_img, bite_list.front() ) );
+				bite_list.pop();
+
+			}
+
+		}
+
+	}				
+
+
+
