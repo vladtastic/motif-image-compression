179894f48d3709fad621529d4c1b45eb19b1ec29
vladimir.coxall <vladimir.coxall@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-11-19 19:00:41 +0000

    OMP Motif.h

---
+++ trunk/Motif-omp.h
@@ -0,0 +1,30 @@
+#ifndef MOTIF_H
+#define MOTIF_H
+
+#include "Image.h"
+#include <vector>
+#include <utility>
+#include <omp.h>
+
+using namespace std;
+
+typedef pair<unsigned int, unsigned int> Coordinate;
+
+class Motif
+{
+	public:
+		vector<Coordinate> Occurrences;
+
+		vector< vector<int> > p;
+
+		unsigned int HEIGHT;
+		unsigned int WIDTH;
+		
+		Motif();
+		~Motif();
+		void Meet(const Image&, const Image&);
+		bool operator==(const Motif&);
+};
+
+
+#endif
