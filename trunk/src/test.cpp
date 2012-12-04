04fa008eb996b71296fc20b2278cd8d1223af757
frozenkida@gmail.com <frozenkida@gmail.com@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-12-04 01:36:53 +0000

    calculated consensus

--- trunk/src/test.cpp
+++ trunk/src/test.cpp
@@ -5,6 +5,65 @@
 
 using namespace std;
 
+
+void get_auto(Image &I)
+{
+	//Get the bites of the image;
+	int x = 100;
+	int y = 100;
+	
+	uint8_t* pix = I.p;
+	uint8_t* bite = (uint8_t *)malloc((I.HEIGHT-y)*(I.WIDTH-x));
+	for(int i = y ; i < I.HEIGHT ; i++)
+	{
+		for(int j = x ; j < I.WIDTH ; j++)
+		{
+			bite[(i-y)*(I.WIDTH-x) + (j-x)] = pix[i * I.WIDTH + j];
+		} 
+	}
+	
+	/*for(int i = 0 ; i < I.HEIGHT - y ; i++)
+	{
+		for(int j = 0 ; j < I.WIDTH - x ; j++)
+		{
+			cout << (unsigned int)pix[i*(I.WIDTH) + j] << "\t";
+		}
+		cout << "\n";
+	}*/
+	
+	/*for(int i = 0 ; i < I.HEIGHT - y ; i++)
+	{
+		for(int j = 0 ; j < I.WIDTH - x ; j++)
+		{
+			cout << (unsigned int)bite[i*(I.WIDTH-x) + j] << "\t";
+		}
+		cout << "\n";
+	}*/
+	
+	//Calculate the consensus between the bite and the image
+	uint8_t* consensus = (uint8_t *)malloc((I.HEIGHT-y)*(I.WIDTH-x));
+	
+	for(int i = 0 ; i < I.HEIGHT - y ; i++)
+	{
+		for(int j = 0; j < I.WIDTH - x ; j++)
+		{
+			if(bite[i*(I.WIDTH-x) + j] == pix[i*(I.WIDTH)+j])
+			{
+				consensus[i*(I.WIDTH-x)+j] = pix[i*(I.WIDTH)+j];
+			}
+			else
+			{	
+				consensus[i*(I.WIDTH-x)+j] = (uint8_t)255; // Don't care character;
+			}
+			
+			cout << (unsigned int)consensus[i*(I.WIDTH - x) + j] << "\t";			
+			
+		}
+		cout << "\n";
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
 	if(argc != 2)
@@ -15,16 +74,7 @@ int main(int argc, char* argv[])
 	char* filename = argv[1];
 	Image I(filename); 
 	
-	uint8_t *pix = I.p;
-	
-	for(int i = 0; i < I.HEIGHT ; i++)
-	{
-		for(int j = 0 ; j < I.WIDTH ; j++)
-		{
-			cout << (unsigned int)pix[i * I.WIDTH + j] << "\t";
-		}
-		cout << "\n";
-	}
+	get_auto(I);
 	
 	return 0 ;	
 }
