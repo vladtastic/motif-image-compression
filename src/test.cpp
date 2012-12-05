63145d62ffff0a281396a98fb7330601dff29f1f
lynermohan <lynermohan@ed561387-bf45-68e5-351c-9aedf59d1484>
2012-12-05 08:52:03 +0000

    Working serial version of occurrence list generator

--- src/test.cpp
+++ src/test.cpp
@@ -62,6 +62,71 @@ void get_auto(Image &I)
 	return;
 }
 
+void get_occurrence(Image &I)
+{
+  // Declare sizes
+  int MW = 100; // Motif width
+  int MH = 100; // Motif height
+  int IW = I.WIDTH; // Image width
+  int IH = I.HEIGHT; // Image height
+  int I_Size = IW*IH; // Size of image
+  int M_Size = IW*(MH-1) + MW; // Size of padded motif
+  int P_Size = I_Size - M_Size + 1; // Size of product string
+
+  // Calculate pattern matches for each possible alphabet characters ***
+  uint8_t* Pattern_Match[128]; // One pattern match for each alphabet
+  for(uint8_t i = 0; i < 128; i++)
+  {
+    Pattern_Match[i] = (uint8_t*)malloc(I_Size);
+    for(int j = 0; j < I_Size; j++)
+    {
+      Pattern_Match[i][j] = ((uint8_t)I.p[j] == (uint8_t)i) ? (uint8_t)0 : (uint8_t)1;
+    }
+  }
+
+  // Make Motif string
+  uint8_t* Image = I.p; // Pointer to the Image
+  uint8_t* Motif = (uint8_t *)malloc(M_Size);
+  int x = IW - MW; // Starting location of bite
+  int y = IH - MH;
+  int k = 0;
+  for (int j = y; j < IH-1; j++) // Loop through all but the last unpaddded row
+  {
+    for (int i = x; i < IW; i++)
+    {
+      Motif[k++] = Image[j*IW + i];
+    }
+    for (int i = 0; i < x; i++) // Padding
+    {
+      Motif[k++] = (uint8_t)255;
+    }
+  }
+  for (int i = x; i < IW; i++) // Last row
+  {
+    Motif[k++] = Image[(IH-1)*IW + i];
+  }
+
+  // Calculate product string
+  uint8_t* Product = (uint8_t *)calloc(P_Size,1);
+  for(int i = 0; i < M_Size; i++)
+  {
+    if(Motif[i] == (uint8_t)255)  // Skip if current pixel is a don't care
+      continue;
+    for(int k = 0; k < P_Size; k++)
+    {
+      Product[k] = (Product[k] == (uint8_t)1) ? (uint8_t)1 : Pattern_Match[Motif[i]][k+i]; // Conditional statement to prevent overflow
+    }
+  }
+  
+  for(int k = 0; k < P_Size; k++)
+  { 
+    if(Product[k] == (uint8_t)0 && k%I.WIDTH <= x)
+    {
+      cout << k % I.WIDTH << "\t" << k / I.WIDTH << endl;
+    }
+  }
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -78,7 +143,9 @@ int main(int argc, char* argv[])
 
 	gettimeofday( &t0, NULL );	
 	
-	get_auto(I);
+	//get_auto(I);
+
+  get_occurrence(I);
 	
 	gettimeofday( &t1, NULL );
 	
